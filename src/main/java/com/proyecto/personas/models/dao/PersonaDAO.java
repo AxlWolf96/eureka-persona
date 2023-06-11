422b0de31d0013d14f523d9e45cb4c75380c664a
Eduardo <eduardo.pullo.pisco@gmail.com>
2023-06-11 12:06:45 -0500

    [ADD] se añade dependencia de lombok

--- src/main/java/com/proyecto/personas/models/dao/PersonaDAO.java
+++ src/main/java/com/proyecto/personas/models/dao/PersonaDAO.java
@@ -4,5 +4,5 @@ import com.proyecto.personas.models.entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonaDAO extends CrudRepository<Persona, Long> {
+public interface PersonaDAO extends JpaRepository<Persona, Long> {
 }
--- src/main/java/com/proyecto/personas/models/entity/Persona.java
+++ src/main/java/com/proyecto/personas/models/entity/Persona.java
@@ -1,9 +1,15 @@
 package com.proyecto.personas.models.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="personas")
 public class Persona implements Serializable {
@@ -28,78 +34,6 @@ public class Persona implements Serializable {
     @Column(name = "numero_celular")
     private String numeroCelular;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidoPaterno() {
-        return apellidoPaterno;
-    }
-
-    public void setApellidoPaterno(String apellidoPaterno) {
-        this.apellidoPaterno = apellidoPaterno;
-    }
-
-    public String getApellidoMaterno() {
-        return apellidoMaterno;
-    }
-
-    public void setApellidoMaterno(String apellidoMaterno) {
-        this.apellidoMaterno = apellidoMaterno;
-    }
-
-    public String getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(String fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public String getTipoDocumento() {
-        return tipoDocumento;
-    }
-
-    public void setTipoDocumento(String tipoDocumento) {
-        this.tipoDocumento = tipoDocumento;
-    }
-
-    public String getNumeroDocumento() {
-        return numeroDocumento;
-    }
-
-    public void setNumeroDocumento(String numeroDocumento) {
-        this.numeroDocumento = numeroDocumento;
-    }
-
-    public String getNumeroCelular() {
-        return numeroCelular;
-    }
-
-    public void setNumeroCelular(String numeroCelular) {
-        this.numeroCelular = numeroCelular;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public String toString() {
